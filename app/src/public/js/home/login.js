b4813521661e8cc6a2246c63b4b389cc54eb13a4
Boram Lee <cnn93@naver.com>
2022-02-15 00:45:24 +0900

    third commit

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -14,17 +14,29 @@ function login() {
 
   axios.post("/login", {
     id: id.value,
-    password: password.value
+    password: password.value,
+    // body: JSON.stringify(req)
   })
-  .then((res) => {
-    if(res.success) {
-      location.href = "/";
-    } else {
-      if(res.err) return alert(res.err);
-      alert(res.msg);
-    };
-  })
-  .catch((err) => {
-    console.error(new Error("로그인 중 에러 발생!"));
+//   const req = {
+//     id: id.value,
+//     password: password.value
+// };
+//   fetch("/login", { // 로그인을 요청하는 fetch, 서버에 전달
+//     method: "POST",
+//     headers: {
+//         "Content-Type": "application/json" // 내가 보내는 데이터의 타입
+//     }, // JSON 데이터라고 알려줌
+//     body: JSON.stringify(req) // req를 문자열로 바꿔줌
+// })
+  .then((res) => { console.log(res);
+  //   if(res.success) {
+  //     location.href = "/";
+  //   } else {
+  //     if(res.err) return alert(res.err);
+  //     alert(res.msg);
+  //   };
+  // })
+  // .catch((err) => {
+  //   console.error(new Error("로그인 중 에러 발생!"));
   });
 };
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -23,13 +23,24 @@ const users = {
 const process = {
     login: (req, res) => {
         // console.log(req.body); // { id: 'd', password: 'd' }
-        if(users.id.includes(id)) { // includes : id가 존재하는지 여부 boolean
+        const id = req.body.id,
+            password = req.body.password;
+
+        if(users.id.includes(id)) { // includes : id가 존재하는지 여부 boolean, 프론트엔드에서 전달받은 id가 user에 있으면
             const idx = users.id.indexOf(id); // 해당 아이디가 몇 번째에 있는지
             // id가 존재한다면 그 id가 몇 번째에 있는지!
-            if(users.password[idx] === password) { // password의 idx(몇번째)와 프론트엔드에서 전달받은 password가 같다면
 
+            if(users.password[idx] === password) { // id와 같은 위치에 있는 password
+            // password의 idx(몇번째)와 프론트엔드에서 전달받은 password가 같다면
+                return res.json({ // 프론트엔드로 응답
+                    success: true,
+                });
             }
         }
+        return res.json({
+            success: false,
+            msg: "로그인에 실패하였습니다."
+        });
     },
 
     register: (req, res) => {
