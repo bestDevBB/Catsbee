e147922abd136946e3cbf1192654e7bd91e5f9b7
Boram <cnn93@naver.com>
2022-02-17 20:42:52 +0900

    commit

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -6,10 +6,6 @@ const router = express.Router();
 const ctrl = require("./home.ctrl.js");
 
 // 라우터
-// router.get("/", (req, res) => {
-//     res.render("home/index.ejs"); // 컨트롤러
-//     // 앱 세팅에서 ./views로 지정해줘서 그 하위폴더인 Home부터
-// });
 router.get("/", ctrl.output.home);
 router.get("/login", ctrl.output.login);
 router.get("/register", ctrl.output.register);
